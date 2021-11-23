05a08e792435d301e2287b9f3c6e2ae6e3b0278a
Cambuchi <cam.manh.nguyen@gmail.com>
2021-11-23 00:52:47 -0800

    Add HtmlWebpackPlugin for automated index.html and configure webpack.config.js to automatically clean the dist directory.

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Crow's Cafe Diner",
+    }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
